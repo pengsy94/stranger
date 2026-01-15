2822dbbe67df1436ee1fe10b00dccc3540207642
pengsy <songyang410@outlook.com>
2026-01-15 15:40:06 +0800

    refactor: 自定义按钮允许合并class

--- src/components/ui/CommonButton.tsx
+++ src/components/ui/CommonButton.tsx
@@ -1,13 +1,19 @@
+import { cn } from "@/lib/utils"
 
 interface IProps {
     label: string
     onClick?: () => void
+    className?: string
 }
 
-const Button = ({ label, onClick = () => { } }: IProps) => {
+const Button = ({ label, onClick = () => { }, className }: IProps) => {
     return (
         <div
-            className="text-white text-center font-medium bg-(--button-background) rounded-sm select-none cursor-pointer shadow-(--shadow-custom-teal) active:bg-(--online-color) md:hover:bg-(--online-color) focus:bg-(--button-primary-color) focus-visible:bg-(--button-primary-color)"
+            className={cn(
+                "text-white text-center font-medium bg-(--button-background) rounded-sm select-none cursor-pointer",
+                "shadow-(--shadow-custom-teal) active:bg-(--online-color) md:hover:bg-(--online-color) focus:bg-(--button-primary-color) focus-visible:bg-(--button-primary-color)",
+                className
+            )}
             onClick={() => onClick()}
         >
             <div className="pl-2 pr-2 pt-1.5 pb-1.5 text-sm">{label}</div>
