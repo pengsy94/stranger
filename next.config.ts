87cbecfd570bd8c543b8d07ba39c57eaab07fc34
pengsy <songyang410@outlook.com>
2026-01-15 13:17:17 +0800

    fix: 开发阶段内网请求的跨域问题

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  /* 开发阶段内网跨域问题 */
+  allowedDevOrigins: ['192.168.*.*'],
   /* 静态模式模式、指定输出文件、关闭图片优化 */
   output: 'export',
   distDir: 'out',
